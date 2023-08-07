f6b72cf332dfb1529b5781c8823964c57273aef2
D3Vofin <137174160+D3Vofin@users.noreply.github.com>
2023-08-07 14:55:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var firstnma="Ranbir";
+var lastnmare= "Kapoor";
+
+console.log (firstnma);
+console.log (lastnmare);
+
+var NOTFULLNAMEANDTHISISNOTTHESAMEVARIABLE=firstnma.concat(lastnmare);
+console.log(NOTFULLNAMEANDTHISISNOTTHESAMEVARIABLE);
+var length=NOTFULLNAMEANDTHISISNOTTHESAMEVARIABLE.length;
+console.log(length);
+
+
+
+
+var sqrroot=Math.sqrt(1234567890987654321);
+console.log(sqrroot);
+
+var maxinum=Math.max(1234789876543209876789, 98765432198765432109876)
+console.log(maxinum);
